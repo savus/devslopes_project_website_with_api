7c43a4a3df9ae9ced31915506ba17b8c54b8c750
Savus <justin.fuzzygreenstuff@gmail.com>
2023-04-25 03:22:29 -0600

    Add data set attributes to filter links and apply onclick event listeners to set active css class.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 
 
+const dataFilter = '[data-filter]';
+const filterClass = '.filter-link';
+const active = 'active';
+
+/* card list */ 
+
+const filterLinks = document.querySelectorAll(dataFilter);
+
+const setActive = (elm, selector) => {
+   if (document.querySelector(`${selector}.${active}`) !== null) {
+      document.querySelector(`${selector}.${active}`).classList.remove(active);
+   }
+   elm.classList.add(active);
+};
+
+
+for (const link of filterLinks) {
+   link.addEventListener('click', function() {
+      setActive(link, filterClass);
+   });   
+}
