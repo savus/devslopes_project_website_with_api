007743988ab7ba579836336eddc5238cad409309
Savus <justin.fuzzygreenstuff@gmail.com>
2023-04-30 01:53:53 -0600

    Add functionality to change mode button to toggle between 2d card mode and carousel mode

--- js/main.js
+++ js/main.js
@@ -23,8 +23,28 @@ for (const link of filterLinks) {
 
 /* carousel */ 
 
-const slides = document.querySelectorAll('.info-card');
-const slideButtons = document.querySelectorAll('.slide-ctrl-container button');
+// info-card-grid carousel-mode
+
+const carouselToggle = '[data-toggle]';
+const infoCard = '.info-card';
+const slideButton = '.slide-ctrl-container button';
+const cardContainer = '.info-card-grid';
+const carouselMode = 'carousel-mode';
+
+const slides = document.querySelectorAll(infoCard);
+const slideButtons = document.querySelectorAll(slideButton);
+const toggleCarousel = document.querySelector(carouselToggle);
+const changeMode = document.querySelector(cardContainer);
+
+toggleCarousel.addEventListener('click', function() {
+   const array = Array.from(changeMode.classList);
+   if (array.includes(carouselMode)) {
+      changeMode.classList.remove(carouselMode);
+   } else {
+      changeMode.classList.add(carouselMode);  
+   }
+});
+
 let currentSlide = Math.floor(Math.random() * slides.length);
 let prevSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
 let nextSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
